7cdc94ad89a3d56b35b2b442525e495377e22ebd
Raju Mazumder <rajuniit@gmail.com>
2014-03-24 13:54:24 +0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LmPilot::Application.config.secret_key_base = 'dfdcc135123ae960f72cf25f6525d14c470f1fb959bcb94ee1de2682b4d4b043ae9a436e319df36c316740f7305dbd118ae582bc24d839f3916fd1da3e2920fb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LmPilot::Application.config.session_store :cookie_store, key: '_lm-pilot_session'
